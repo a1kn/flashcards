eac1c643addac1659c143bf346fcb34a18e96c1d
Arthur Kauffman <arthur@sent.com>
2021-02-05 10:17:22 -0500

    housekeeping

--- src/models/flashcards.js
+++ src/models/flashcards.js
@@ -3,6 +3,7 @@ const db = require('./../../data/db');
 module.exports = {
   get() {
     const flashcards = db.select(
+      'flashcards.id',
       'flashcards.title', 
       'flashcards_languages.content'
     ).from('flashcards').join(
