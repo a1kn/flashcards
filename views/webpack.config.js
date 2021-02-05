d737f46106b41e9be56ab996735fe03c2a631607
Arthur Kauffman <arthur@sent.com>
2021-02-05 10:15:51 -0500

    initial react setup

---
+++ views/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const htmlWebpack = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index_bundle.js'
+  },
+  module: {
+    rules: [
+      { 
+        test: /\.(js)$/, 
+        use: 'babel-loader' 
+      },
+      { 
+        test: /\.css$/, 
+        use: ['style-loader', 'css-loader'] 
+      }
+    ]
+  },
+  mode: 'development',
+  plugins: [
+    new htmlWebpack({
+        template: 'src/index.html'
+    })
+  ]
+};
